5c376b5be908ab34d021ded1db32016510a33192
NotTahaAli <tahaali.6074+github@gmail.com>
2026-03-25 10:49:08 +0500

    feat: Basic Main Activity & Home Fragment

--- app/src/main/java/com/l230954/cinefast/MainActivity.java
+++ app/src/main/java/com/l230954/cinefast/MainActivity.java
@@ -1,41 +1,24 @@
 package com.l230954.cinefast;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class OnboardingActivity extends AppCompatActivity {
-
-    Button btnGetStarted;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onboarding);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        init();
-
-        btnGetStarted.setOnClickListener((v)->{
-            Intent i = new Intent(this, MainActivity.class);
-            startActivity(i);
-            finish();
-        });
-    }
-
-    private void init() {
-        btnGetStarted = findViewById(R.id.btnGetStarted);
     }
 }
