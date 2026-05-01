ef6ba517aeca5578fbef50969caaedef6cf548a8
NotTahaAli <tahaali.6074+github@gmail.com>
2026-05-01 15:48:51 +0500

    Login and Signup Screens without Functionality

---
+++ app/src/main/java/com/l230954/cinefast/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.l230954.cinefast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageView btnBack;
+    TextView tvRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        init();
+
+        btnBack.setOnClickListener(v -> finish());
+        
+        tvRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void init() {
+        btnBack = findViewById(R.id.btnBack);
+        tvRegister = findViewById(R.id.tvRegister);
+    }
+}
